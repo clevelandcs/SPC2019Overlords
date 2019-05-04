63acf021f78862c486857c6a11163f99a5c95cb5
Andrew Wojciechowski <wojciechowskia@msoe.edu>
2019-05-04 10:21:30 -0500

    Adding in jumbled password problem

---
+++ SSEProgrammingCompetition/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        string line1 = Console.ReadLine();
+        string line2 = Console.ReadLine();
+        int output = 0;
+        if (line1.Length > line2.Length)
+            output += line1.Length - line2.Length;
+        else
+            output += line2.Length - line1.Length;
+        output += line2.Where(x => !line1.Contains(x)).Count();
+        Console.WriteLine(output);
+        Console.ReadKey();
+    }
+}
